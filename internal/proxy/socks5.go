92a07affb114b2ff4b2840c376655fc74163834f
Wonkeun <asdasfafsf@naver.com>
2026-04-04 18:36:50 +0900

    [WON-69] feat: SOCKS5 프록시에 Pipeline 연결 (#75)
    NewSOCKS5Proxy에 pipeline 파라미터를 추가하고
    Engine에서 pipeline을 전달하도록 변경.

--- internal/proxy/socks5.go
+++ internal/proxy/socks5.go
@@ -12,6 +12,7 @@ import (
 
 	"coroxy/internal/adapter"
 	"coroxy/internal/constant"
+	"coroxy/internal/intercept"
 	"coroxy/internal/model"
 
 	"github.com/google/uuid"
@@ -33,13 +34,15 @@ const (
 type SOCKS5Proxy struct {
 	logger    *slog.Logger
 	onSession adapter.SessionCallback
+	pipeline  *intercept.Pipeline
 }
 
 // NewSOCKS5Proxy creates a new SOCKS5 proxy handler.
-func NewSOCKS5Proxy(logger *slog.Logger, onSession adapter.SessionCallback) *SOCKS5Proxy {
+func NewSOCKS5Proxy(logger *slog.Logger, onSession adapter.SessionCallback, pipeline *intercept.Pipeline) *SOCKS5Proxy {
 	return &SOCKS5Proxy{
 		logger:    logger,
 		onSession: onSession,
+		pipeline:  pipeline,
 	}
 }
 
